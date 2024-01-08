5e8539fecac869d07c0e5ee9b442bd884fa04f8d
Umutcan Oğuz <147054825+1230505039@users.noreply.github.com>
2024-01-08 18:11:40 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	
+	int term_number;
+	
+	printf("Enter the term number: ");
+	scanf("%d",&term_number);
+	
+	float terms[term_number];
+	
+	int i;
+	
+	for(i = 0 ; i < term_number ; i++){
+		printf("Enter the %d term: ",i + 1);
+		scanf("%f", &terms[i]);
+	}
+	
+	float tangent_x_point;
+	
+	printf("Enter the x's point of tangent: ");
+	scanf("%f", &tangent_x_point);
+	
+	int j;
+	printf("Your equation: ");
+	for(i = 0, j = term_number - 1 ; i < term_number, j >= 0 ; i++, j--){
+		if(j == 0){
+			printf("%.1f", terms[i], j);
+
+		}
+		else{
+		
+		printf(" %.1fx^%d + ", terms[i], j);
+		}
+	}
+	
+	float tangent_y_point = 0;
+	
+	for(i = 0, j = term_number - 1 ; i < term_number, j >= 0 ; i++, j--){
+		tangent_y_point = tangent_y_point + terms[i]*pow(tangent_x_point, j);
+	}
+	
+	printf("\n\n");
+	
+	printf("The equation of tangent: ");
+	
+	for(i = 0, j = term_number - 2 ; i < term_number, j >= 0 ; i++, j--){
+		terms[i] = terms[i]*(j + 1);
+		
+		if(j == 0){
+			printf("%.1f", terms[i]);
+		}
+		
+		else{
+		printf("%.1fx^%d + ", terms[i], j);
+		}
+	}
+	
+	
+	return 0;
+}
